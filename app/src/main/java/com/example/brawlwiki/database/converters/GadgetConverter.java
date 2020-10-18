5ed4af2d27497bc3caca343f066c33c206f49f15
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2020-10-17 23:12:00 -0700

    feat: Added two converter classes for StarPower.java and Gadget.java

---
+++ app/src/main/java/com/example/brawlwiki/database/converters/GadgetConverter.java
@@ -0,0 +1,30 @@
+package com.example.brawlwiki.database.converters;
+
+import androidx.room.TypeConverter;
+
+import com.example.brawlwiki.models.brawlers.Gadget;
+import com.example.brawlwiki.models.brawlers.StarPower;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class GadgetConverter {
+
+    @TypeConverter
+    public static List<Gadget> fromStringToStarPower(String value) {
+
+        Type listType = new TypeToken<List<Gadget>>() {}.getType();
+
+        return new Gson().fromJson(value, listType);
+    }
+
+    @TypeConverter
+    public static String fromListToString(List<Gadget> list) {
+
+        Gson gson = new Gson();
+
+        return gson.toJson(list);
+    }
+}
