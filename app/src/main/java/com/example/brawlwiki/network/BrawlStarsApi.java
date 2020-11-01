4c230fcaa01236b701c1fc7dfb5126b594d151c5
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2020-11-01 00:45:41 -0700

    fix: Changed headers from being static to be taken programmatically

--- app/src/main/java/com/example/brawlwiki/network/BrawlStarsApi.java
+++ app/src/main/java/com/example/brawlwiki/network/BrawlStarsApi.java
@@ -2,63 +2,39 @@ package com.example.brawlwiki.network;
 
 import com.example.brawlwiki.models.brawlers.BrawlerList;
 import com.example.brawlwiki.models.clubranking.ClubMemberList;
-import com.example.brawlwiki.models.gamemodes.GamesModeList;
-import com.example.brawlwiki.models.gamemodes.maps.MapsList;
 import com.example.brawlwiki.models.playerranking.PlayerRankingList;
 import com.example.brawlwiki.models.profile.Player;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.Headers;
 import retrofit2.http.Path;
 
 public interface BrawlStarsApi {
 
     //Get request for players personal in game data
-    @Headers({
-            "Accept: application/json",
-            "authorization: Bearer " + ApiClient.API_KEY
-    })
     @GET("v1/players/{tag}")
-    Call<Player> getPlayer(@Path("tag") String tag);
+    Call<Player> getPlayer(
+            @Header("authorization") String token,
+            @Path("tag") String tag
+    );
 
     //Get request for a list of top clubs and their ranks
-    @Headers({
-            "Accept: application/json",
-            "authorization: Bearer " + ApiClient.API_KEY
-    })
     @GET("v1/rankings/global/clubs?before=eyJwb3MiOjEwMH0&limit=100")
-    Call<ClubMemberList> getClubList();
+    Call<ClubMemberList> getClubList(
+            @Header("authorization") String token
+    );
 
     //Get request for a list of top players and their ranks
-    @Headers({
-            "Accept: application/json",
-            "authorization: Bearer " + ApiClient.API_KEY
-    })
     @GET("v1/rankings/global/players?before=eyJwb3MiOjEwMH0&limit=100")
-    Call<PlayerRankingList> getTopPlayerList();
-
-    //Get request for all Game Modes in game
-    @Headers({
-            "Accept: application/json",
-            "authorization: Bearer " + ApiClient.API_KEY
-    })
-    @GET("api/gamemodes.json")
-    Call<GamesModeList> getGameModes();
-
-    //Get request for all Maps in game
-    @Headers({
-            "Accept: application/json",
-            "authorization: Bearer " + ApiClient.API_KEY
-    })
-    @GET("api/maps.json")
-    Call<MapsList> getMaps();
+    Call<PlayerRankingList> getTopPlayerList(
+            @Header("authorization") String token
+    );
 
     //Get request for all brawlers in game
-    @Headers({
-            "Accept: application/json",
-            "authorization: Bearer " + ApiClient.API_KEY
-    })
     @GET("v1/brawlers")
-    Call<BrawlerList> getBrawlers();
+    Call<BrawlerList> getBrawlers(
+            @Header("authorization") String token
+    );
 }
