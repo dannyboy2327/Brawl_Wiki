94234b603d132e7bbb71eb5f5008e01198d2aadf
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2020-11-02 23:56:05 -0800

    fix: Fixed an issue by adding if statement checks on users tag for tablets that are on landscape config

--- app/src/main/java/com/example/brawlwiki/ui/settings/settingsoptions/ProfileSettingsFragment.java
+++ app/src/main/java/com/example/brawlwiki/ui/settings/settingsoptions/ProfileSettingsFragment.java
@@ -2,16 +2,19 @@ package com.example.brawlwiki.ui.settings.settingsoptions;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.preference.PreferenceManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.brawlwiki.R;
 import com.example.brawlwiki.TokenLoginActivity;
@@ -28,14 +31,9 @@ public class ProfileSettingsFragment extends Fragment {
         mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_profile_settings, container, false);
         View root = mBinding.getRoot();
 
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
-        String tag = sharedPreferences.getString("MyTag", "");
-        if (!tag.equals("")) {
-            mBinding.tvPlayerTag.setText("#" + sharedPreferences.getString("MyTag", "").substring(1));
-        } else {
-            mBinding.tvPlayerTag.setText(sharedPreferences.getString("MyTag", ""));
-        }
-        mBinding.tvPlayerToken.setText(sharedPreferences.getString("MyToken", ""));
+        setPlayerTag();
+
+        setStatusOfToken();
 
         mBinding.btResetTag.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,10 +52,37 @@ public class ProfileSettingsFragment extends Fragment {
         return root;
     }
 
+    private void setPlayerTag() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
+        String tag = sharedPreferences.getString("MyTag", "");
+        if (!tag.equals("")) {
+            mBinding.tvPlayerTag.setText("#" + sharedPreferences.getString("MyTag", "").substring(1));
+        } else {
+            mBinding.tvPlayerTag.setText("No tag is set.");
+        }
+    }
+
+    private void setStatusOfToken() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
+        String token = sharedPreferences.getString("MyToken", "");
+        if (token.equals("")) {
+            mBinding.tvPlayerToken.setText("No token is set.");
+        } else {
+            mBinding.tvPlayerToken.setText("Token is set.");
+        }
+    }
+
     private void deletePlayerToken() {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
-        sharedPreferences.edit().remove("MyToken").apply();
-        goToTokenLoginActivity();
+        String token = sharedPreferences.getString("MyToken", "");
+
+        if (token.equals("")) {
+            Toast.makeText(getContext(), "No token has been set", Toast.LENGTH_SHORT).show();
+        } else {
+            sharedPreferences.edit().remove("MyToken").apply();
+            goToTokenLoginActivity();
+        }
+
     }
 
     private void goToTokenLoginActivity() {
@@ -67,7 +92,23 @@ public class ProfileSettingsFragment extends Fragment {
 
     private void deletePlayerTag() {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
-        sharedPreferences.edit().remove("MyTag").apply();
-        Navigation.findNavController(getView()).navigate(R.id.nav_home);
+        String tag = sharedPreferences.getString("MyTag", "");
+
+        if (tag.equals("")) {
+            Toast.makeText(getContext(), "No tag has been set", Toast.LENGTH_SHORT).show();
+        }
+         else {
+            sharedPreferences.edit().remove("MyTag").apply();
+
+            boolean isTablet = getResources().getBoolean(R.bool.isTablet);
+            int orientation = getResources().getConfiguration().orientation;
+            if (isTablet && orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment);
+                navController.navigate(R.id.nav_home);
+                Navigation.findNavController(getView()).navigate(R.id.nav_settings);
+            } else {
+                Navigation.findNavController(getView()).navigate(R.id.nav_home);
+            }
+        }
     }
 }
