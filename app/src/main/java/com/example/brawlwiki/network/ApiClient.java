ee6d37380e1a349944c8087123f667e6eee1ec9c
dannyboy2327 <dtorizvaldovinos@ucmerced.edu>
2020-11-04 14:49:19 -0800

    fix: Removed a method that wasn't being used

--- app/src/main/java/com/example/brawlwiki/network/ApiClient.java
+++ app/src/main/java/com/example/brawlwiki/network/ApiClient.java
@@ -6,7 +6,6 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ApiClient {
 
     public static final String BRAWL_STARS_BASE_URL = "https://api.brawlstars.com/";
-    public static final String BRAWL_LIST_BASE_URL = "https://cdn.starlist.pro/";
 
     public static Retrofit getBrawlStarsClient() {
         Retrofit retrofit = null;
@@ -18,16 +17,4 @@ public class ApiClient {
         }
         return retrofit;
     }
-
-    public static Retrofit getBrawlListClient() {
-        Retrofit retrofit = null;
-        if (retrofit == null) {
-            retrofit = new Retrofit.Builder()
-                    .baseUrl(BRAWL_LIST_BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
-        }
-        return retrofit;
-    }
-
 }
